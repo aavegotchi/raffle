af6062deb73e1605ac6b597844fd49f2e5daebe5
Nick Mudge <nick@perfectabstractions.com>
2020-10-23 20:34:27 -0400

    Deployed raffled

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
-# solidity-contracts
-Template for setting up a contracts repo.
+# Raffle and Vouchers
+
+Kovan addresses
+- Deployed VouchersContract: 0x1aec38e26b5c1ff7CFf788b8eb0054da1a8Ba841
+- Deployed RaffleContract: 0xa127CCDaBa75F3c1bA6223127B290121e99Dd097
--- scripts/startRaffle.js
+++ scripts/startRaffle.js
@@ -11,9 +11,10 @@ async function main () {
   let vouchersContract
   let rafflesContract
   if (hre.network.name === 'kovan') {
+    ;[prizeAddress, rafflesAddress] = await deployContracts()
     stakeAddress = '0xA4fF399Aa1BB21aBdd3FC689f46CCE0729d58DEd'
-    vouchersContract = await ethers.getContractAt('VouchersContract', '')
-    rafflesContract = await ethers.getContractAt('RafflesContract', '')
+    vouchersContract = await ethers.getContractAt('VouchersContract', prizeAddress)
+    rafflesContract = await ethers.getContractAt('RafflesContract', rafflesAddress)
   } else if (hre.network.name === 'hardhat') {
     ;[prizeAddress, rafflesAddress] = await deployContracts()
     stakeAddress = '0xA4fF399Aa1BB21aBdd3FC689f46CCE0729d58DEd'
@@ -23,8 +24,53 @@ async function main () {
     throw Error('No network settings for ' + hre.network.name)
   }
 
-  await vouchersContract.createVoucherTypes(account, [1000, 1000, 1000, 500, 500, 500, 300, 300, 300, 150, 150, 150, 50, 50, 50, 5, 5], '0x')
+  const prizes = []
+  const raffleItems = []
+  let prizeId = 0
+  let prizeValue
+  for (let stakeId = 0; stakeId < 6; stakeId++) {
+    if (stakeId === 0) {
+      prizeValue = 1000
+    } else if (stakeId === 1) {
+      prizeValue = 500
+    } else if (stakeId === 2) {
+      prizeValue = 300
+    } else if (stakeId === 3) {
+      prizeValue = 150
+    } else if (stakeId === 4) {
+      prizeValue = 50
+    } else if (stakeId === 5) {
+      prizeValue = 5
+    }
+    let jLength
+    if (stakeId === 5) {
+      jLength = 2
+    } else {
+      jLength = 3
+    }
+    for (let j = 0; j < jLength; j++) {
+      prizes.push(prizeValue)
+      raffleItems.push({
+        stakeAddress: stakeAddress,
+        stakeId: stakeId,
+        prizeAddress: prizeAddress,
+        prizeId: prizeId,
+        prizeValue: prizeValue
+      })
+      prizeId++
+    }
+  }
+  await vouchersContract.createVoucherTypes(account, prizes, '0x')
   console.log('Created voucher types and minted vouchers')
+  await vouchersContract.setApprovalForAll(rafflesContract.address, true)
+  console.log('Approved raffleContract to transfer vouchers')
+  const now = new Date()
+  const secondsSinceEpoch = Math.round(now.getTime() / 1000)
+  const aWeek = 604800// 604800 == 1 week
+  await rafflesContract.startRaffle(secondsSinceEpoch + aWeek, raffleItems)
+  console.log('Started raffle')
+  console.log('Here are the raffle items:')
+  console.log(raffleItems)
 }
 
 // We recommend this pattern to be able to use async/await everywhere
