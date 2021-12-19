873552d5bafdc3cc58b44613884ae7e0165d7225
Coderdan <dmathieuva@gmail.com>
2021-12-19 16:05:06 +0800

    Update verifyContract.ts
    update verify contract

--- scripts/verifyContract.ts
+++ scripts/verifyContract.ts
@@ -2,13 +2,13 @@
 import { run } from "hardhat";
 
 export async function main() {
-  const voucherIds = ["0", "1", "2", "3"];
-  const quantities = ["1217", "1217", "373", "193"];
-
-  const constructorArgs = [voucherIds, quantities];
+  const constructorArgs = [
+    "0x96ac66A3BF1305e06c722e37f8a2706c4E38Bc77",
+    "0x1D0360BaC7299C86Ec8E99d0c1C9A95FEfaF2a11",
+  ];
 
   await run("verify:verify", {
-    address: "0x96ac66A3BF1305e06c722e37f8a2706c4E38Bc77",
+    address: "0xd5724BCA82423D5792C676cd453c1Bf66151dC04",
     constructorArguments: constructorArgs,
   });
 }
