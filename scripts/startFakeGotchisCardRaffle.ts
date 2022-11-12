3986fd9003f6667f78e639dabe0441497a99c80d
Coderdan <dmathieuva@gmail.com>
2022-11-12 21:14:06 +0800

    Update startFakeGotchisCardRaffle.ts

--- scripts/startFakeGotchisCardRaffle.ts
+++ scripts/startFakeGotchisCardRaffle.ts
@@ -7,7 +7,7 @@ export async function main() {
   const fakeGotchisCard = "0x9f6BcC63e86D44c46e85564E9383E650dc0b56D7";
   const itemManager = "0x8D46fd7160940d89dA026D59B2e819208E714E82";
   const prizeAmount = "1500";
-  const raffleDuration = "72"; //in hours
+  const raffleDuration = (3600 * 72 + 1800).toString(); //in seconds
   const voucherId = "0";
 
   // Then start the raffle, using the voucher address as the prize
